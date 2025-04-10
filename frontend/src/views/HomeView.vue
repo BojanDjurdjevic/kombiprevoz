97da6ee59b88866051bf9a5520354f38b27072af
BojanDjurdjevic <pininfarina164@gmail.com>
2025-04-10 18:10:43 +0200

    Landing content

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,13 +4,25 @@
 
 <template>
   <v-container
-    class="fill-height fill-width pa-0"
+    class="fill-height fill-width pa-0 overflow-hidden position-relative"
     fluid
   >
     <v-img
-      src="https://picsum.photos/350/165?random"
+      src="https://picsum.photos/3500/1650?random"
       height="100%"
       cover
     ></v-img>
+
+    <v-container fluid 
+      class="fill-height fill-width bg-black opacity-50
+      position-absolute top-9 left-0
+    "></v-container>
+    <v-container fluid
+      class="fill-height fill-width text-white
+      position-absolute top-9 left-0 d-flex justify-center"
+    >
+      <h1>Pozdrav</h1>
+    </v-container>
   </v-container>
+
 </template>
