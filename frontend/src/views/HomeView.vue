8584bec45e13ab433f91509c1ffee9807daafc31
BojanDjurdjevic <pininfarina164@gmail.com>
2025-04-14 20:59:08 +0200

    Landing Content

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -20,8 +20,8 @@
     <v-container fluid
       class="text-white
       position-absolute top-9 left-0 
-      h-75
-      d-flex justify-space-around align-center flex-wrap"
+      h-85
+      d-flex flex-column flex-md-row justify-space-around align-center"
     >
       <v-container>
         <h1>Kombi Prevoz Putnika</h1>
@@ -35,10 +35,10 @@
         <h2>sa vaše adrese</h2>
       </v-container>
       <v-container
-        class="d-flex justify-end"
+        class="d-flex flex-column justify-space-around align-end"
       >
         <v-card height="18rem" width="18rem" elevation="9" to="gradovi" 
-            class="rounded-xl"
+            class="rounded-xl ma-3"
         >
             <v-img
                 class="align-end text-white"
@@ -51,6 +51,22 @@
                     <source srcset="../assets/img/minibus.jpg">
                 </template>
                 <v-card-title class="text-center">Naša vozila</v-card-title>
+            </v-img> 
+        </v-card>
+        <v-card height="18rem" width="18rem" elevation="9" to="destinacije" 
+            class="rounded-xl ma-3"
+        >
+            <v-img
+                class="align-end text-white"
+                height="100%"
+                        
+                cover
+                src="https://picsum.photos/id/551/3500/1650"
+            >
+                <template #sources>
+                    <source srcset="https://picsum.photos/id/551/3500/1650">
+                </template>
+                <v-card-title class="text-center">Destinacije</v-card-title>
             </v-img> 
         </v-card>
       </v-container>
