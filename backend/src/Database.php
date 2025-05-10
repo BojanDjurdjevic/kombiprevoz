5d7b67e25e652d3a835af79d5e478e364283e191
BojanDjurdjevic <pininfarina164@gmail.com>
2025-05-10 07:59:51 +0200

    File config

---
+++ backend/src/Database.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class Database {
+    private $conn;
+
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO('mysql:host=' . $_ENV["DATABASE_HOSTNAME"] . ';dbname='. $_ENV["DATABASE_NAME"], $_ENV["DATABASE_USERNAME"], $_ENV["DATABASE_PASSWORD"]);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Connection Error: '. $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+
+}
+
+?>
