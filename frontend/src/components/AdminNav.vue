dc977a30e3232b226f653ced560b076f285b56b1
BojanDjurdjevic <pininfarina164@gmail.com>
2025-09-26 15:46:59 +0200

    Admin Panel Filters adding

--- frontend/src/components/AdminNav.vue
+++ frontend/src/components/AdminNav.vue
@@ -70,7 +70,9 @@ import { useAdminStore } from '@/stores/admin';
                 <h1 class="mt-3"> {{ admin.adminView }} </h1>
                 <v-divider class="w-100"></v-divider>
             </div>
-            
+            <div class="main">
+
+            </div> 
         </v-container>
         <v-divider vertical></v-divider>
         <v-container class="w-25 pa-3 d-flex flex-column justify-space-between align-center">
@@ -78,16 +80,85 @@ import { useAdminStore } from '@/stores/admin';
                 <h1 class="mt-3"> Filters </h1>
                 <v-divider class="w-100"></v-divider>
             </div>
-            <div class="pa-6 w-100 h-100 d-flex flex-column ">
-              <div>
-                <v-date-input
-                  v-model="admin.depDay.date"
-                  label="Datum vožnje"
-                >
+            <v-container class="w-100 h-100 d-flex flex-column justify-space-between align-center"
+              v-if="admin.adminView == 'Bookings'"
+            >
+              <div class="pa-6 w-100 h-100 d-flex flex-column ">
+                <div class="ma-4">
+                  <v-text-field
+                    prepend-icon="mdi-book-open-variant"
+                    v-model="admin.bCode"
+                    label="Broj rezervacije"
+                    clearable
+                  >
 
-                </v-date-input>
+                  </v-text-field>
+                </div>
+                <div class="ma-4">
+                  <v-date-input
+                    v-model="admin.depDay.date"
+                    label="Datum vožnje"
+                  >
+
+                  </v-date-input>
+                </div>
+                <div class="ma-4">
+                  <v-select
+                    prepend-icon="mdi-highway"
+                    clearable
+                    :items="admin.tours"
+                    item-title="name"
+                    item-value="id"
+                    label="Izaberi Rutu"
+                    v-model="admin.tourID"
+                    return-object
+                  ></v-select>
+                </div>
               </div>
-            </div>
+              <div class="w-100 h-25 d-flex flex-column align-center justify-space-evenly">
+                <v-btn 
+                  class="w-75"
+                  prepend-icon="mdi-magnify"
+                  color="green-darken-3"
+                >Traži</v-btn>
+                <v-btn 
+                  class="w-75"
+                  prepend-icon="mdi-close-circle-multiple"
+                  color="red-darken-3"
+                >Obriši sve</v-btn>
+              </div>
+            </v-container>
+
+            <!--    USERS    -->
+            
+            <v-container class="w-100 h-100 d-flex flex-column justify-space-between align-center"
+              v-if="admin.adminView == 'Users'"
+            >
+              <div class="pa-6 w-100 h-100 d-flex flex-column ">
+                <div class="ma-4">
+                  <v-text-field
+                    prepend-icon="mdi-email"
+                    v-model="admin.usrEmail"
+                    label="Email korisnika"
+                    clearable
+                  >
+
+                  </v-text-field>
+                </div>
+              </div>
+              <div class="w-100 h-25 d-flex flex-column align-center justify-space-evenly">
+                <v-btn 
+                  class="w-75"
+                  prepend-icon="mdi-magnify"
+                  color="green-darken-3"
+                >Traži</v-btn>
+                <v-btn 
+                  class="w-75"
+                  prepend-icon="mdi-close-circle-multiple"
+                  color="red-darken-3"
+                >Obriši sve</v-btn>
+              </div>
+            </v-container>
         </v-container>
         
         </v-main>
--- frontend/src/stores/admin.js
+++ frontend/src/stores/admin.js
@@ -10,6 +10,8 @@ import { useRoute } from 'vue-router';
 export const useAdminStore = defineStore('admin', () => {
 
     const adminView = ref('Bookings')
+
+    // BOOKINGS
     const depDay = ref({
         date: null,
         range: null
@@ -18,8 +20,21 @@ export const useAdminStore = defineStore('admin', () => {
     const bCode = ref(null)
     const driverID = ref(null)
 
+    const tours = ref([
+        {id: 1, name: 'Novi Sad - Rijeka'},
+        {id: 2, name: 'Rijeka - Novi Sad'}
+    ])
+
+    // USERS
+    const usrEmail = ref(null)
+
+    // TOURS
+    const tourName = ref(null)
+    const toursFrom = ref(null)
+    const toursTo = ref(null)
+
     const actions = ref({
-        searchByFilter: () => {
+        searchBooking: () => {
             const dto = {
                 departure: depDay.value,
                 tour_id: tourID.value,
@@ -27,12 +42,27 @@ export const useAdminStore = defineStore('admin', () => {
                 driver_id: driverID.value
             }
             console.log(dto)
+        },
+        searchUser: () => {
+            const dto = {
+                email: usrEmail.value
+            }
+            console.log(dto)
+        },
+        searchTour: () => {
+            const dto = {
+                title: tourName.value,
+                t_from: toursFrom.value,
+                t_to: toursTo.value
+            }
+            console.log(dto)
         }
     })
 
 
     return {
-        adminView, depDay, tourID, bCode, driverID,
+        adminView, depDay, tourID, bCode, driverID, tours, usrEmail, tourName, toursFrom, toursTo,
+        
     }
 
 })
