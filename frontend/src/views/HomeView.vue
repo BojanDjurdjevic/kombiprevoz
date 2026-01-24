574e99f7e4962e44ab440e622e7173ee1c2486e0
BojanDjurdjevic <pininfarina164@gmail.com>
2026-01-24 18:35:24 +0100

    Vehicle imgs added

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,84 +1,6 @@
 <script setup>
 
 import minibusImg from '@/assets/img/minibus.jpg';
-
-  /**
-   
-<template>
-  <v-container
-    class="fill-height fill-width pa-0 overflow-hidden position-relative"
-    fluid
-  >
-    <v-img
-      src="https://picsum.photos/id/314/3500/1650"
-      height="100%"
-      cover
-    ></v-img>
-
-    <v-container fluid 
-      class="fill-height fill-width bg-black opacity-30
-      position-absolute top-9 left-0
-    "></v-container>
-    <v-container fluid
-      class="text-white
-      position-absolute top-9 left-0 
-      h-85
-      d-flex flex-column flex-md-row justify-space-around align-center"
-    >
-      <v-container>
-        <h1>Kombi Prevoz Putnika</h1>
-        <br />
-        <h4>SIGURNO I UDOBNO</h4>
-        <br />
-        <h2>Najbolji kombi prevoz do</h2>
-        <br />
-        <h2 color="pink-darken-4">Inostranstva</h2>
-        <br />
-        <h2>sa vaše adrese</h2>
-      </v-container>
-      <v-container
-        class="d-flex flex-column justify-space-around align-end"
-      >
-        <v-card height="18rem" width="18rem" elevation="9" to="gradovi" 
-            class="rounded-xl ma-3"
-        >
-            <v-img
-                class="align-end text-white"
-                height="100%"
-                        
-                cover
-                src="../assets/img/minibus.jpg"
-            >
-                <template #sources>
-                    <source srcset="../assets/img/minibus.jpg">
-                </template>
-                <v-card-title class="text-center">Naša vozila</v-card-title>
-            </v-img> 
-        </v-card>
-        <v-card height="18rem" width="18rem" elevation="9" to="destinacije" 
-            class="rounded-xl ma-3"
-        >
-            <v-img
-                class="align-end text-white"
-                height="100%"
-                        
-                cover
-                src="https://picsum.photos/id/551/3500/1650"
-            >
-                <template #sources>
-                    <source srcset="https://picsum.photos/id/551/3500/1650">
-                </template>
-                <v-card-title class="text-center">Destinacije</v-card-title>
-            </v-img> 
-        </v-card>
-      </v-container>
-    </v-container>
-  </v-container>
-
-</template>
-
-   */
-
 </script>
 
 <template>
@@ -136,7 +58,7 @@ import minibusImg from '@/assets/img/minibus.jpg';
         <v-card
           class="rounded-xl mb-4"
           elevation="8"
-          to="/gradovi"
+          to="/vozila"
         >
           <v-img
             :src="minibusImg"
--- frontend/src/views/Vehicles.vue
+++ frontend/src/views/Vehicles.vue
@@ -1,24 +1,33 @@
 <script setup>
-    const vehicles = [] 
+    const vehicles = [
+        { src: '../../public/vans/van1.jpg' },
+        { src: '../../public/vans/van2.jpg' },
+        { src: '../../public/vans/van3.jpg' },
+        { src: '../../public/vans/van4.jpg' },
+        { src: '../../public/vans/van5.jpg' },
+        { src: '../../public/vans/van6.jpg' },
+        { src: '../../public/vans/van7.jpg' }
+    ] 
 
 </script>
 
 <template>
     <v-container fluid class="p-6">
-      <v-row justify="center" class="mb-6" v-if="dest.city">
+      <v-row justify="center" class="mb-6">
         <v-col cols="12" md="10">
             <v-carousel
                 show-arrows-on-hover
-                height="300"
+                height="500"
                 hide-delimiter-background
             >
             <v-carousel-item
-                v-for="(item, i) in dest.cityPics"
+                v-for="(item, i) in vehicles"
                 :key="i"
-                :src="item"
+                :src="item.src"
+                eager
                 cover
-                :alt="`Slika grada ${dest.city} - ${i+1}`"
-                lazy-src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                aspect-ratio="16/9"
+                alt="Slika vozila za prevoz putnika"
             />
             </v-carousel>
         </v-col>
